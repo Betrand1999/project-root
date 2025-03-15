40260d962bac9cc7eb5ae85693acdec953673e58
BETRAND <mmutagha@gmail.com>
2025-03-15 17:50:00 -0400

    add k8s deployment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-cloud-devops-consulting',
-    version='0.1.4',
+    version='0.1.5',
     author='Betrand Mutagha',
     author_email='mmutagha@gmail.com',
     description='This is my consulting website for Cloud & DevOps services.',
