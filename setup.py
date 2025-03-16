90e581b019c1aabb8dbcb5bc9614594b6a3c6bc4
BETRAND <mmutagha@gmail.com>
2025-03-16 00:36:24 -0400

    added pod

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-cloud-devops-consulting',
-    version='0.1.21',
+    version='0.1.22',
     author='Betrand Mutagha',
     author_email='mmutagha@gmail.com',
     description='This is my consulting website for Cloud & DevOps services.',
