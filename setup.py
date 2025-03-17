7bd169fce9108b5cd410c5998887d65a28321a9f
BETRAND <mmutagha@gmail.com>
2025-03-16 20:48:08 -0400

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='my-cloud-devops-consulting',
-    version='0.1.25',
+    version='0.1.26',
     author='Betrand Mutagha',
     author_email='mmutagha@gmail.com',
     description='This is my consulting website for Cloud & DevOps services.',
