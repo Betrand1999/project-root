a33d46146133c5eb835ec805fd1c6ad04b50d023
BETRAND <mmutagha@gmail.com>
2025-03-23 14:25:48 -0400

    added code

--- settings.py
+++ settings.py
@@ -2,18 +2,14 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SECRET_KEY=os.environ.get('SECRET_KEY','your_secret_key')
-EMAIL_USER=os.environ.get('EMAIL_USER', 'default_email_user')
-EMAIL_PASS=os.environ.get('EMAIL_PASS', 'default_email_password')
-SMTP_SERVER=os.environ.get('SMTP_SERVER', 'default_smtp_server')
-SMTP_PORT=os.environ.get('SMTP_PORT', 587)  # Default SMTP port for TLS
 MONGO_URI=os.environ.get('MONGO_URI')
 SECRET_KEY=os.environ.get('SECRET_KEY','your_secret_key')
 EMAIL_USER=os.environ.get('EMAIL_USER', 'default_email_user')
 EMAIL_PASS=os.environ.get('EMAIL_PASS', 'default_email_password')
 SMTP_SERVER=os.environ.get('SMTP_SERVER', 'default_smtp_server')
 SMTP_PORT=os.environ.get('SMTP_PORT', 587)  # Default SMTP port for TLS
-MONGO_URI=os.environ.get('MONGO_URI')
 AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION=os.getenv("AWS_REGION")
+MONGO_USERNAME=os.getenv('MONGO_USERNAME')
+MONGO_PASSWORD=os.getenv('MONGO_PASSWORD')
